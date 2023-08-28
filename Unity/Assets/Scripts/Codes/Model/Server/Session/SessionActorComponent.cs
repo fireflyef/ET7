42ba2843e473f37cd5b7cda1bfe7c6a830250984
wenlong.yang <wenlong.yang@funplus.com>
2023-08-28 15:40:08 +0800

    [ET7] refactor 精简ET代码移除Demo内容

---
+++ Unity/Assets/Scripts/Codes/Model/Server/Session/SessionActorComponent.cs
@@ -0,0 +1,8 @@
+﻿namespace ET.Server
+{
+    [ComponentOf(typeof(Session))]
+    public class SessionActorComponent : Entity, IAwake, IDestroy
+    {
+        public long ActorId { get; set; }
+    }
+}
